0d060bfe46d1b0a6fe20c3c99e504d672f04239d
lilinze <512049503@qq.com>
2024-12-07 17:30:29 +0800

    feat: modify setting

--- internal/setting/setting.go
+++ internal/setting/setting.go
@@ -8,34 +8,61 @@ import (
 
 var Config *AppConfig
 
+// AppConfig 包含整个配置文件的结构
 type AppConfig struct {
-	App      App      `mapstructure:"app"`
-	Database Database `mapstructure:"database"`
+	App          App          `mapstructure:"app"`
+	Database     Database     `mapstructure:"database"`
+	Redis        Redis        `mapstructure:"redis"`
+	DesignGPTAPI DesignGPTAPI `mapstructure:"design_gpt_api"`
 }
 
+// App 配置
 type App struct {
-	Port int `mapstructure:"port"`
+	Port        int      `mapstructure:"port"`
+	SecretKey   string   `mapstructure:"secret_key"`
+	CORSOrigins []string `mapstructure:"cors_origins"`
 }
 
-type Database struct {
+// MySQL 配置
+type MySQL struct {
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
 	Host     string `mapstructure:"host"`
 	Port     int    `mapstructure:"port"`
-	User     string `mapstructure:"user"`
-	Password string `mapstructure:"password"`
 	Name     string `mapstructure:"name"`
+}
+
+// Database 配置，包含多个数据库配置
+type Database struct {
+	MySQL          MySQL `mapstructure:"mysql"`
+	DesignGPTMySQL MySQL `mapstructure:"design_gpt_mysql"`
+}
+
+// Redis 配置
+type Redis struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db"`
+}
+
+// DesignGPTAPI 配置
+type DesignGPTAPI struct {
+	BaseURL string `mapstructure:"base_url"`
 }
 
 // LoadConfig 从 viper 加载配置到 Config 结构体
 func LoadConfig(env string) {
-	// 相对路径
-	configFile := fmt.Sprintf("internal/.env/%s.yaml", env)
+	// 配置文件路径
+	configFile := fmt.Sprintf("internal/config/%s.yaml", env)
 	viper.SetConfigFile(configFile)
 
 	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading .env file %s: %v", configFile, err)
+		log.Fatalf("Error reading config file %s: %v", configFile, err)
 	}
-	log.Printf("Using .env file: %s", configFile)
+	log.Printf("Using config file: %s", configFile)
 
+	// 将配置映射到 Config 结构体
 	if err := viper.Unmarshal(&Config); err != nil {
 		log.Fatalf("Unable to decode into struct: %v", err)
 	}
