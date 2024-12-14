1ddbc6f7b8d565d8db3ea78d3797e800a32d3aa4
lilinze <512049503@qq.com>
2024-12-14 15:11:25 +0800

    feat: add trace middle

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -2,31 +2,36 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"time"
 )
 
 // LoggerMiddleware 创建一个Gin中间件，使用Logrus记录请求日志
 func LoggerMiddleware(logger *logrus.Logger) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// 将Logger注入上下文
-		c.Set("logger", logger)
+	return func(ctx *gin.Context) {
+		traceID := ctx.GetHeader("x-trace-id")
+		if traceID == "" {
+			traceID = uuid.New().String()
+		}
+		ctx.Header("x-trace-id", traceID)
 
 		startTime := time.Now()
 		// 处理请求
-		c.Next()
+		ctx.Next()
 		// 记录请求日志
 		duration := time.Since(startTime)
 		entry := logger.WithFields(logrus.Fields{
-			"status_code": c.Writer.Status(),
-			"method":      c.Request.Method,
-			"path":        c.Request.URL.Path,
-			"query":       c.Request.URL.RawQuery,
-			"ip":          c.ClientIP(),
-			"user_agent":  c.Request.UserAgent(),
+			"status_code": ctx.Writer.Status(),
+			"method":      ctx.Request.Method,
+			"path":        ctx.Request.URL.Path,
+			"query":       ctx.Request.URL.RawQuery,
+			"ip":          ctx.ClientIP(),
+			"user_agent":  ctx.Request.UserAgent(),
 			"duration":    duration.Seconds(),
+			"trace_id":    traceID,
 		})
-		if len(c.Errors) > 0 {
+		if len(ctx.Errors) > 0 {
 			entry.Error("请求处理错误")
 		} else {
 			entry.Info("请求处理完成")
