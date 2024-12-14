03e1d0c53330397c9984eae037417fcb8248b9bf
lilinze <512049503@qq.com>
2024-12-14 16:32:44 +0800

    feat: modify logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -14,33 +14,3 @@ func Logger(ctx *gin.Context) *logrus.Entry {
 
 	return BaseLogger.WithField("trace_id", traceID)
 }
-
-// Info 记录 Info 级别日志，带 ctx 上下文
-func Info(ctx *gin.Context, message string) {
-	Logger(ctx).Info(message)
-}
-
-// Infof 记录 Info 级别日志，带格式化和 ctx 上下文
-func Infof(ctx *gin.Context, format string, args ...interface{}) {
-	Logger(ctx).Infof(format, args...)
-}
-
-// Error 记录 Error 级别日志，带 ctx 上下文
-func Error(ctx *gin.Context, message string) {
-	Logger(ctx).Error(message)
-}
-
-// Errorf 记录 Error 级别日志，带格式化和 ctx 上下文
-func Errorf(ctx *gin.Context, format string, args ...interface{}) {
-	Logger(ctx).Errorf(format, args...)
-}
-
-// Warn 记录 Warn 级别日志，带 ctx 上下文
-func Warn(ctx *gin.Context, message string) {
-	Logger(ctx).Warn(message)
-}
-
-// Warnf 记录 Warn 级别日志，带格式化和 ctx 上下文
-func Warnf(ctx *gin.Context, format string, args ...interface{}) {
-	Logger(ctx).Warnf(format, args...)
-}
